ed2d83133df4bb6e5f61d359cff9a2a73618082d
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-10 17:17:07 +0000

    Alert component and action dispatch

--- src/actions/alert.js
+++ src/actions/alert.js
@@ -1,9 +1,13 @@
 import { v4 as uuidV4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
+  const id = uuidV4();
+
   dispatch({
     type: SET_ALERT,
-    payload: { id: uuidV4(), msg, alertType }
+    payload: { id, msg, alertType }
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+import { setAlert } from './../../actions/alert';
+
+const Register = props => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -19,7 +23,7 @@ const Register = () => {
     e.preventDefault();
 
     if (password !== password2) {
-      console.log('Passwords do not match');
+      props.setAlert('Passwords do not match!', 'danger');
     } else {
       console.log(formData);
     }
@@ -87,4 +91,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
