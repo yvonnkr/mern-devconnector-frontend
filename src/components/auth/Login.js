2617b6dd733c3acd925d33d1dbc7fa1309a8b185
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-10 12:59:07 +0000

    Login form initial setup

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onChangeHandler = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitHandler = e => {
+    e.preventDefault();
+
+    console.log('form submitted');
+  };
+
   return (
-    <div>
-      <h1>login</h1>
-    </div>
+    <>
+      {/* <div className='alert alert-danger'>Invalid credentials</div> */}
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Sign into Your Account
+      </p>
+      <form className='form' onSubmit={e => onSubmitHandler(e)}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChangeHandler(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={e => onChangeHandler(e)}
+            required
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </>
   );
 };
 
