259072bb2b21a985639e486c1ed33f34d3de475a
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-12 16:00:13 +0000

    ProfileExperience,ProfileEducation & ProfileGithub

--- src/components/profile/ProfileEducation.js
+++ src/components/profile/ProfileEducation.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Moment from 'react-moment';
 
-const ProfileEducation = () => {
+const ProfileEducation = ({ education }) => {
+  const { school, degree, fieldofstudy, to, from, description } = education;
   return (
     <div>
-      <h1>ProfileEducation</h1>
+      <h3 className='text-dark'>{school}</h3>
+      <p>
+        <Moment format='YYYY/MM/DD'>{from}</Moment> -
+        {!to ? 'Now' : <Moment format='YYYY/MM/DD'>{to}</Moment>}
+      </p>
+      <p>
+        <strong>Degree: </strong>
+        {degree}
+      </p>
+      <p>
+        <strong>Field Of Study: </strong>
+        {fieldofstudy}
+      </p>
+      <p>
+        <strong>Description: </strong>
+        {description}
+      </p>
     </div>
   );
 };
--- src/components/profile/ProfileExperience.js
+++ src/components/profile/ProfileExperience.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Moment from 'react-moment';
 
-const ProfileExperience = () => {
+const ProfileExperience = ({ experience }) => {
+  const { company, title, to, from, description } = experience;
   return (
     <div>
-      <h1>ProfileExperience</h1>
+      <h3 className='text-dark'>{company}</h3>
+      <p>
+        <Moment format='YYYY/MM/DD'>{from}</Moment> -
+        {!to ? 'Now' : <Moment format='YYYY/MM/DD'>{to}</Moment>}
+      </p>
+      <p>
+        <strong>Position: </strong>
+        {title}
+      </p>
+      <p>
+        <strong>Description: </strong>
+        {description}
+      </p>
     </div>
   );
 };
