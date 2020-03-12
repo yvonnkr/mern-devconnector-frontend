827414214aa4b4882b782748b5e1af00c8cfb6d5
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-12 15:48:14 +0000

    ProfileTop and ProfileAbout Components

--- src/components/profile/ProfileAbout.js
+++ src/components/profile/ProfileAbout.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const ProfileAbout = () => {
+const ProfileAbout = ({ profile }) => {
+  const {
+    bio,
+    skills,
+    user: { name }
+  } = profile;
+
   return (
-    <div>
-      <h1>Profile About</h1>
+    <div className='profile-about bg-light p-2'>
+      {bio && (
+        <Fragment>
+          <h2 className='text-primary'>{name.trim().split(' ')[0]}'s Bio</h2>
+          <p>{bio}</p>
+        </Fragment>
+      )}
+
+      <div className='line' />
+      <h2 className='text-primary'>Skill Set</h2>
+      <div className='skills'>
+        {skills.map((skill, index) => (
+          <div className='p-1' key={index}>
+            <i className='fa fa-check' />
+            {skill}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
