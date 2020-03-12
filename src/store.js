6a17e36d3033701e4764efd8e90359a8e1b8d3f3
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-12 20:24:40 +0000

    disable devTools in production

--- src/store.js
+++ src/store.js
@@ -7,10 +7,17 @@ import rootReducer from './reducers/root-reducer';
 const initialState = {};
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
+
+const devTools =
+  process.env.NODE_ENV === 'production'
+    ? applyMiddleware(...middleware)
+    : composeWithDevTools(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, initialState, devTools);
 
 export default store;
