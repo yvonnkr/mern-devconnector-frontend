c4037bdfda88db64a6d9da79b669a5050711efab
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-12 13:13:21 +0000

    finished profile actions / reducer

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   UPDATE_PROFILE,
   ACCOUNT_DELETED,
-  CLEAR_PROFILE
+  CLEAR_PROFILE,
+  GET_REPOS
 } from './types';
 import { setAlert } from './alert';
 
@@ -18,6 +20,59 @@ export const getCurrentProfile = () => async dispatch => {
     dispatch({
       type: GET_PROFILE,
       payload: response.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Get all profiles
+export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+
+  try {
+    const res = await axios.get(`${url}/api/profile`);
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Get profile by Id
+export const getProfileById = userId => async dispatch => {
+  try {
+    const res = await axios.get(`${url}/api/profile/user/${userId}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Get GitHub Repos
+export const getGithubRepos = username => async dispatch => {
+  try {
+    const res = await axios.get(`${url}/api/profile/github/${username}`);
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
     });
   } catch (err) {
     dispatch({
